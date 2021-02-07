4bb0a05035a8a76d009870527264ba66892f009e
Jorge Alfaro <jorge.alfmur@gmail.com>
2021-02-07 10:13:35 -0500

    create a cookie and redirect to another route

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, request, make_response, redirect
 app = Flask(__name__)
 
 
 @app.route('/')
+def index():
+    user_ip = request.remote_addr
+    response = make_response(redirect('/hello'))
+    response.set_cookie('user_ip', user_ip)
+    return response
+
+
+@app.route('/hello')
 def hello_world():
-    return 'Hello, World!!!'
+    user_ip = request.cookies.get('user_ip')
+    return f'Hola tu dirección IP es: {user_ip}'
 
 
 if __name__ == '__main__':
