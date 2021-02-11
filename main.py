759a35255870fcce653a9435b8747946bb7cfe02
Jorge Alfaro <jorge.alfmur@gmail.com>
2021-02-11 18:07:10 -0500

    install flask-marshmallow

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
 
 @app.route('/')
